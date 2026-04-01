0e312ca952d3582dae04f4766aa63c692a4507ee
Leon Sorokin <leeoniya@gmail.com>
2026-03-31 19:31:04 -0500

    useGetState

--- useRefState/App.js
+++ useRefState/App.js
@@ -13,3 +13,20 @@ function App() {
     }
   }, data ? data[0].fields[1].values[0] : 'No data!');
 }
+
+// using a getter
+function App2() {
+  const [getData, setData] = useGetState();
+
+  // if (getData()) {
+  //   setInterval(() => {
+  //     setData(getData());
+  //   }, 100);
+  // }
+
+  return React.createElement("button", {
+    onClick: () => {
+      setData(genFrames())
+    }
+  }, getData() ? getData()[0].fields[1].values[0] : 'No data!');
+}
--- useRefState/useRefState.js
+++ useRefState/useRefState.js
@@ -4,8 +4,8 @@ function useRefState(thing) {
   const [, forceUpdate] = React.useReducer(incr, 0);
 
   const ref = React.useRef();
-
   ref.current ??= [
+    // thing itself, cannot be passed or memo'd without incurring 2x memory
     thing,
     newThing => {
       if (newThing !== ref.current[0]) {
@@ -14,6 +14,31 @@ function useRefState(thing) {
       }
     }
   ];
+
+  thing = null;
+
+  return ref.current;
+}
+
+function useGetState(thing) {
+  const [, forceUpdate] = React.useReducer(incr, 0);
+
+  const _ref = React.useRef();
+  _ref.current ??= thing;
+
+  const ref = React.useRef();
+  ref.current ??= [
+    // thing getter, can be drilled and passed around without
+    () => _ref.current,
+    newThing => {
+      if (newThing !== _ref.current) {
+        _ref.current = newThing;
+        forceUpdate();
+      }
+    }
+  ];
+
+  thing = null;
 
   return ref.current;
 }
